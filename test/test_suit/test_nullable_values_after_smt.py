e74e0b5bd59c11ccc564e6c3a0bfcb572fd48f36
Lex Shcharbaty <147443583+sfc-gh-lshcharbaty@users.noreply.github.com>
2024-03-20 14:55:37 +0100

    SNOW-1055524 E2E test with nullable SMT (#808)

--- test/test_suit/test_nullable_values_after_smt.py
+++ test/test_suit/test_nullable_values_after_smt.py
@@ -4,7 +4,6 @@ from test_suit.test_utils import NonRetryableError, RetryableError
 
 
 # Testing behavior for behavior.on.null.values = IGNORE and SMTs that can return null values.
-# Uses a Snowpipe based connector.
 class TestNullableValuesAfterSmt:
 
     def __init__(self, driver, nameSalt):
@@ -13,14 +12,18 @@ class TestNullableValuesAfterSmt:
         self.table = self.fileName + nameSalt
         self.topic = self.table
 
-        self.total_events = 200
+        self.driver.snowflake_conn.cursor().execute(
+            f'create or replace table {self.table} (index number not null)')
+
+        self.driver.snowflake_conn.cursor().execute(
+            f'alter table {self.table} set enable_schema_evolution = true')
 
     def getConfigFileName(self):
         return self.fileName + '.json'
 
     def send(self):
         value = []
-        for idx in range(self.total_events):
+        for idx in range(200):
             event = { 'index': idx, 'someKey': 'someValue' }
 
             if idx % 2 == 0: # Only every other event contains optionalField.
@@ -33,23 +36,25 @@ class TestNullableValuesAfterSmt:
 
     def verify(self, round):
         cur = self.driver.snowflake_conn.cursor(DictCursor)
-        res = cur.execute(f'select record_content, record_metadata:offset::number as offset from {self.table}').fetchall()
+        res = cur.execute(f'select index, from_optional_field, record_metadata:offset::number as offset from {self.table}').fetchall()
 
         if len(res) == 0:
             raise RetryableError()
         elif len(res) != 100:
             raise NonRetryableError('Number of record in table is different from number of expected records')
 
-        # Originally RECORD_CONTENT is returned as a json string.
-        parsed_res = [{'RECORD_CONTENT': json.loads(rec['RECORD_CONTENT']), 'OFFSET': rec['OFFSET']} for rec in res]
+        idx = 0
+        for rec in res:
+            if rec['INDEX'] != idx:
+                raise NonRetryableError(f"Invalid index value. Expected: {idx}, got: {rec['INDEX']}")
 
-        expected_idx = range(0, self.total_events, 2) # Only every other event is going to be ingested.
-        expected_res = [{'RECORD_CONTENT': {'index': idx, 'from_optional_field': True}, 'OFFSET': idx} for idx in expected_idx]
+            if not rec['FROM_OPTIONAL_FIELD']:
+                raise NonRetryableError('Invalid from_optional_field value. Expected to be true')
 
-        if expected_res != parsed_res:
-            raise NonRetryableError(f"Invalid result values. Expected: {expected_res}, got: {parsed_res}")
+            if rec['OFFSET'] != idx:
+                raise NonRetryableError(f"Invalid offset value. Expected: {idx}, got: {rec['OFFSET']}")
 
-        self.driver.verifyStageIsCleaned(self.topic)
+            idx += 2  # Only every other event is going to be ingested.
 
     def clean(self):
         self.driver.cleanTableStagePipe(self.topic, self.table)
