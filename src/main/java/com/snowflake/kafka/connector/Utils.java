b2e30aa9a6915483cb6ebf562c982a6410b4d6ae
Robert Sawicki <robert.sawicki@snowflake.com>
2024-10-30 15:30:04 +0100

    NO-SNOW Release 2.5.0 (#975)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -70,7 +70,7 @@ import org.apache.kafka.common.utils.ThreadUtils;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.4.1";
+  public static final String VERSION = "2.5.0";
 
   // connector parameter list
   public static final String NAME = "name";
