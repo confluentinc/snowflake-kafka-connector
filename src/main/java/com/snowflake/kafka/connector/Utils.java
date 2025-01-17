68fd3c0b209c4c11d04774f45308eab3060bef88
Sudesh Wasnik <swasnik@confluent.io>
2025-01-17 09:24:32 +0000

    [connect-releases] prepare release v2.2.2-rc-b77e6cf

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -72,7 +72,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.2.2";
+  public static final String VERSION = "2.2.2-rc-b77e6cf";
 
   // connector parameter list
   public static final String NAME = "name";
