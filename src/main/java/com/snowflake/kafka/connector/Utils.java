76144446250b44cbbae29af2f6be4bf99d8c3f6c
Sangeet Kumar Mishra <samishra@confluent.io>
2025-03-05 05:55:30 +0000

    [connect-releases] prepare release v3.1.0-rc-e49efcee

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -65,7 +65,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.1.0";
+  public static final String VERSION = "3.1.0-rc-e49efcee";
 
   // connector parameter list
   public static final String NAME = "name";
