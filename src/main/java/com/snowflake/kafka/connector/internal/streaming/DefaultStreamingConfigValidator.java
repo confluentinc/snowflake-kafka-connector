a0011f256e0eb446e3735f83ce167abcf7ccc8d3
Michał Bobowski <145468486+sfc-gh-mbobowski@users.noreply.github.com>
2025-07-28 18:01:36 +0200

    SNOW-2227742 Add exponential backoff to handle openChannel errors (#1146)

--- src/main/java/com/snowflake/kafka/connector/internal/streaming/DefaultStreamingConfigValidator.java
+++ src/main/java/com/snowflake/kafka/connector/internal/streaming/DefaultStreamingConfigValidator.java
@@ -2,6 +2,11 @@ package com.snowflake.kafka.connector.internal.streaming;
 
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.*;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.INGESTION_METHOD_OPT;
+import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.KEY_CONVERTER_CONFIG_FIELD;
+import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.OPEN_CHANNEL_MAX_RETRY_ATTEMPTS;
+import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.SNOWPIPE_STREAMING_MAX_CLIENT_LAG;
+import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.SNOWPIPE_STREAMING_MAX_MEMORY_LIMIT_IN_BYTES;
+import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.VALUE_CONVERTER_CONFIG_FIELD;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
@@ -110,6 +115,10 @@ public class DefaultStreamingConfigValidator implements StreamingConfigValidator
 
           if (inputConfig.containsKey(TASK_BUFFER_TOTAL_LIMIT_BYTES)) {
             ensureValidLong(inputConfig, TASK_BUFFER_TOTAL_LIMIT_BYTES, invalidParams);
+
+          if (inputConfig.containsKey(OPEN_CHANNEL_MAX_RETRY_ATTEMPTS)) {
+            ensureValidIntWithMinimum(
+                inputConfig, OPEN_CHANNEL_MAX_RETRY_ATTEMPTS, 1, invalidParams);
           }
 
           // Valid schematization for Snowpipe Streaming
@@ -135,6 +144,30 @@ public class DefaultStreamingConfigValidator implements StreamingConfigValidator
           param,
           Utils.formatString(
               param + " configuration must be a parsable long. Given configuration" + " was: {}",
+              inputConfig.get(param)));
+    }
+  }
+
+  private static void ensureValidIntWithMinimum(
+      Map<String, String> inputConfig,
+      String param,
+      int minimumValue,
+      Map<String, String> invalidParams) {
+    try {
+      int value = Integer.parseInt(inputConfig.get(param));
+      if (value < minimumValue) {
+        invalidParams.put(
+            param,
+            Utils.formatString(
+                param + " configuration must be at least {}. Given configuration was: {}",
+                minimumValue,
+                value));
+      }
+    } catch (NumberFormatException exception) {
+      invalidParams.put(
+          param,
+          Utils.formatString(
+              param + " configuration must be a parsable int. Given configuration was: {}",
               inputConfig.get(param)));
     }
   }
