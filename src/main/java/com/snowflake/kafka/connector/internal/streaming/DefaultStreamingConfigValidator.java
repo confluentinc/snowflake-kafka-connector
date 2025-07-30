5cc217edd928a439e153e94080e0b616a1655794
Michał Bobowski <145468486+sfc-gh-mbobowski@users.noreply.github.com>
2025-07-30 11:41:47 +0200

    SNOW-2227742 Retry indefinitely on 429 and 500 (#1149)

--- src/main/java/com/snowflake/kafka/connector/internal/streaming/DefaultStreamingConfigValidator.java
+++ src/main/java/com/snowflake/kafka/connector/internal/streaming/DefaultStreamingConfigValidator.java
@@ -3,7 +3,6 @@ package com.snowflake.kafka.connector.internal.streaming;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.*;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.INGESTION_METHOD_OPT;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.KEY_CONVERTER_CONFIG_FIELD;
-import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.OPEN_CHANNEL_MAX_RETRY_ATTEMPTS;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.SNOWPIPE_STREAMING_MAX_CLIENT_LAG;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.SNOWPIPE_STREAMING_MAX_MEMORY_LIMIT_IN_BYTES;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.VALUE_CONVERTER_CONFIG_FIELD;
@@ -115,11 +114,6 @@ public class DefaultStreamingConfigValidator implements StreamingConfigValidator
 
           if (inputConfig.containsKey(TASK_BUFFER_TOTAL_LIMIT_BYTES)) {
             ensureValidLong(inputConfig, TASK_BUFFER_TOTAL_LIMIT_BYTES, invalidParams);
-
-          if (inputConfig.containsKey(OPEN_CHANNEL_MAX_RETRY_ATTEMPTS)) {
-            ensureValidIntWithMinimum(
-                inputConfig, OPEN_CHANNEL_MAX_RETRY_ATTEMPTS, 1, invalidParams);
-          }
 
           // Valid schematization for Snowpipe Streaming
           invalidParams.putAll(validateSchematizationConfig(inputConfig));
