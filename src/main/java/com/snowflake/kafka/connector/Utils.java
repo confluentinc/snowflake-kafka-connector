474a31b48b31df7efe735305984da16b52e77934
NONE <NONE>
2026-05-22 08:22:31 +0000

    [connect-releases] prepare release v3.1.0-rc-aadbe796

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -70,7 +70,7 @@ import org.apache.kafka.common.utils.ThreadUtils;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.1.0";
+  public static final String VERSION = "3.1.0-rc-aadbe796";
 
   // connector parameter list
   public static final String NAME = "name";
