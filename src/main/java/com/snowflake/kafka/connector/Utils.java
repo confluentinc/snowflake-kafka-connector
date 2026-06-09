249ca80d39211bfc3ec991c848bdabf5ffc043d1
Nirut Gupta <nirgupta@confluent.io>
2026-06-09 17:10:00 +0000

    [connect-releases] prepare release v3.4.0-rc-f1d80ec9

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -72,7 +72,7 @@ import org.apache.kafka.common.utils.ThreadUtils;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.4.0";
+  public static final String VERSION = "3.4.0-rc-f1d80ec9";
 
   // connector parameter list
   public static final String NAME = "name";
