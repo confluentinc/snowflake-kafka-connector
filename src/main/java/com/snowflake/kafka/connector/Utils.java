e5491cee30345bd7a29c38443ef99e2f9d04da7b
Nirut Gupta <nirgupta@confluent.io>
2026-08-28 11:29:53 +0530

    apply format.sh

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -679,8 +679,11 @@ public class Utils {
       String table = tt[1].trim();
 
       if (!isValidSnowflakeTableName(table)) {
-        errorMessage = "Table name " + table + " should have at least 2 characters, " +
-                "start with _a-zA-Z, and only contains _$a-zA-z0-9";
+        errorMessage =
+            "Table name "
+                + table
+                + " should have at least 2 characters, "
+                + "start with _a-zA-Z, and only contains _$a-zA-z0-9";
         LOGGER.error(errorMessage);
         isInvalid = true;
       }
@@ -694,7 +697,8 @@ public class Utils {
       // check that regexes don't overlap
       for (String parsedTopic : topic2Table.keySet()) {
         if (parsedTopic.matches(topic) || topic.matches(parsedTopic)) {
-          errorMessage = "Topic regexes cannot overlap. overlapping regexes: " + parsedTopic + ", " + topic;
+          errorMessage =
+              "Topic regexes cannot overlap. overlapping regexes: " + parsedTopic + ", " + topic;
           LOGGER.error(errorMessage);
           isInvalid = true;
         }
